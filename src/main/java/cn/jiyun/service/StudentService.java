f40dfd957a8cb20fc5479cc31d71c2231b5a2374
Yannick <86963023+YoungYannick@users.noreply.github.com>
2024-07-01 14:32:55 +0800

    Delete src/main directory

--- src/main/java/cn/jiyun/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package cn.jiyun.service;
-
-import cn.jiyun.pojo.Student;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface StudentService extends IService<Student> {
-    Page<Student> findByLimit(Integer pageSize, Integer pageNum, Student student);
-}
